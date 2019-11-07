bf82ab2d7f1292eec9d5fa5056676e6a0e8dfcd5
mihailgaberov <mihail.gaberov@gmail.com>
2019-11-07 16:28:57 +0200

    Added styles.

--- src/App.js
+++ src/App.js
@@ -1,37 +1,67 @@
-import React from "react";
+import React, { useRef } from "react";
+import { Input, MessageList, Button } from "react-chat-elements";
+
+import "./App.css";
 import "react-chat-elements/dist/main.css";
 
-import { Input, MessageBox, Button } from "react-chat-elements";
-
 function App() {
+  const refContainer = useRef("");
+
+  const sendMessage = () => {
+    console.log(refContainer.current.state.value);
+  };
+
   return (
     <div className="App">
-      <MessageBox
-        position={"left"}
-        type={"photo"}
-        text={"mihail_gaberov.svg"}
-        data={{
-          uri:
-            "https://mihail-gaberov.eu/static/profile-pic-538ec3f11211b7561f61b99ab54fb65f.jpg",
-          status: {
-            click: false,
-            loading: 0
+      <div className="App-container">
+        <MessageList
+          className="message-list"
+          lockable={true}
+          toBottomHeight={"100%"}
+          dataSource={[
+            {
+              position: "right",
+              type: "text",
+              text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit",
+              date: new Date()
+            },
+            {
+              position: "left",
+              type: "photo",
+              text: "mihail.svg",
+              date: new Date(),
+              data: {
+                uri:
+                  "https://mihail-gaberov.eu/static/profile-pic-538ec3f11211b7561f61b99ab54fb65f.jpg",
+                status: {
+                  click: false,
+                  loading: 0
+                }
+              }
+            }
+          ]}
+        />
+        <Input
+          placeholder="Type here..."
+          multiline={true}
+          ref={refContainer}
+          rightButtons={
+            <Button
+              color="white"
+              backgroundColor="black"
+              text="Send"
+              onClick={sendMessage}
+            />
           }
-        }}
-      />
-       <Input
-        placeholder="Type here..."
-        multiline={true}
-        rightButtons={
-          <Button color="white" backgroundColor="black" text="Send" />
-        }
-        inputStyle={{ border: '2px solid #dedede',
-          backgroundColor: '#f1f1f1',
-          borderRadius: '5px',
-          padding: '10px',
-          margin: '10px 0'
-        }}
-      />
+          inputStyle={{
+            border: "2px solid #dedede",
+            backgroundColor: "#f1f1f1",
+            borderRadius: "5px",
+            padding: "10px",
+            margin: "10px 0"
+          }}
+        />
+      </div>
     </div>
   );
 }
