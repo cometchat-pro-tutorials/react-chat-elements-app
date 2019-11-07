f74f2d440653ca8af25d5013471ac4a771d82a1d
mihailgaberov <mihail.gaberov@gmail.com>
2019-11-07 12:02:08 +0200

    Styling the chat input.

--- src/App.js
+++ src/App.js
@@ -1,31 +1,35 @@
 import React from "react";
 import "react-chat-elements/dist/main.css";
 
-import { ChatItem, MessageBox } from "react-chat-elements";
+import { Input, MessageBox, Button } from "react-chat-elements";
 
 function App() {
   return (
     <div className="App">
-      <ChatItem
-        avatar={
-          "https://mihail-gaberov.eu/static/profile-pic-538ec3f11211b7561f61b99ab54fb65f.jpg"
-        }
-        alt={"Reactjs"}
-        title={"Facebook"}
-        subtitle={"What are you doing?"}
-        date={new Date()}
-        unread={10}
-      />
       <MessageBox
         position={"left"}
         type={"photo"}
         text={"mihail_gaberov.svg"}
         data={{
-          uri: "https://mihail-gaberov.eu/static/profile-pic-538ec3f11211b7561f61b99ab54fb65f.jpg",
+          uri:
+            "https://mihail-gaberov.eu/static/profile-pic-538ec3f11211b7561f61b99ab54fb65f.jpg",
           status: {
             click: false,
             loading: 0
           }
+        }}
+      />
+       <Input
+        placeholder="Type here..."
+        multiline={true}
+        rightButtons={
+          <Button color="white" backgroundColor="black" text="Send" />
+        }
+        inputStyle={{ border: '2px solid #dedede',
+          backgroundColor: '#f1f1f1',
+          borderRadius: '5px',
+          padding: '10px',
+          margin: '10px 0'
         }}
       />
     </div>
