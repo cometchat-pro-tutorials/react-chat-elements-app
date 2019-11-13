6eda3fb4a474e1d75ef5e40d204ab346c545f80a
mihailgaberov <mihail.gaberov@gmail.com>
2019-11-13 18:32:20 +0200

    Moved the normalization of the past messages data to be executed only once.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { useState } from "react";
-import { fetchChatGroupConversations, initChat, loginChat, logoutChat } from "./chat-api";
+import {
+  fetchChatGroupConversations,
+  initChat,
+  loginChat,
+  logoutChat
+} from "./chat-api";
 import ChatArea from "./components/ChatArea";
 import Login from "./components/Login";
-import { clearAll, readRecord, storeToLocalStorage } from "./utils/localStorageService";
+import {
+  clearAll,
+  readRecord,
+  storeToLocalStorage
+} from "./utils/localStorageService";
 
 import "react-chat-elements/dist/main.css";
 import "./App.css";
-
 
 function App() {
   const [hasName, setHasName] = useState(readRecord("username") !== null);
@@ -19,9 +27,21 @@ function App() {
           .then(data => {
             storeToLocalStorage("username", username);
             setHasName(data.uid === username);
-            fetchChatGroupConversations().then(pastMessages =>
-              setMessages(pastMessages)
-            );
+            fetchChatGroupConversations().then(pastMessages => {
+              pastMessages = pastMessages.map(msg => {
+                // Replace all properties 'type:image' with 'type: photo' as per react-chat-elements needs
+                if (msg.type && msg.type === "image") {
+                  msg.type = "photo";
+                }
+
+                if (msg.data && msg.data.type && msg.data.type === "image") {
+                  msg.data.type = "photo";
+                }
+
+                return msg;
+              });
+              setMessages(pastMessages);
+            });
           })
           .catch(err => console.error("No connection to the Chat API", err))
       )
@@ -35,18 +55,15 @@ function App() {
   };
 
   const handleAddMessage = (groupConversations, msg) => {
-    // Replace all properties 'type:image' with 'type: photo' as per react-chat-elements needs
-    groupConversations = groupConversations.map(msg => {
-      if (msg.type && msg.type === 'image') {
-        msg.type = 'photo';
-      }
+    // Replace the property 'type:image' with 'type: photo' of the current message as per react-chat-elements needs
+    if (msg.type && msg.type === "image") {
+      msg.type = "photo";
+    }
 
-      if (msg.data && msg.data.type && msg.data.type === 'image') {
-        msg.data.type = 'photo';
-      }
+    if (msg.data && msg.data.type && msg.data.type === "image") {
+      msg.data.type = "photo";
+    }
 
-      return msg;
-    });
     setMessages([...groupConversations, ...[msg]]);
   };
 
