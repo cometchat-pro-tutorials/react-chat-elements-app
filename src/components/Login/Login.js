3e77960f5763505ea0a5073463b7e696936d1613
mihailgaberov <mihail.gaberov@gmail.com>
2019-11-23 09:39:18 +0200

    Added cometchat logo on login page.

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./Login.css";
+import Logo from '../../images/cometchat-logo.svg'; 
 
 const Login = ({ callback }) => {
   const [username, setUsername] = useState("");
@@ -32,12 +33,14 @@ const Login = ({ callback }) => {
                 usernames, e.g. "superhero1"
               </div>
             )}
+            <div><a href="https://www.cometchat.com/" target="_blank" rel="noreferrer noopener"><img src={Logo} alt="CometChat logo" /></a></div>
             <label htmlFor="username">Welcome to our <a href="https://github.com/Detaysoft/react-chat-elements" target="_blank" rel="noreferrer noopener">react-chat-elements</a> chat demo powered by CometChat. Login with the username superhero1, superhero2 or superhero3 and test the chat out. To create your own user, see <a href="https://github.com/Detaysoft/react-chat-elements" target="_blank" rel="noreferrer noopener">our documentation.</a></label>
           </div>
           <input
             name="username"
             className="login-input"
             label="Name"
+            placeholder="Enter your username"
             value={username}
             onChange={e => setUsername(e.target.value)}
             margin="normal"
