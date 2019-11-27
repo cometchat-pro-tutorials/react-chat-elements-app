be33930ea0480403462b6c3d0ce950d5763430b9
Mihail Gaberov <mihail.gaberov@gmail.com>
2019-11-27 15:54:38 +0200

    Removed redundant duplicated code.

--- src/App.js
+++ src/App.js
@@ -48,56 +48,6 @@ function App() {
             });
             return true;
           })
-          .catch(err => {import React, { useState, useEffect } from "react";
-import {
-  fetchChatGroupConversations,
-  initChat,
-  loginChat,
-  logoutChat
-} from "./chat-api";
-import ChatArea from "./components/ChatArea";
-import Login from "./components/Login";
-import {
-  clearAll,
-  readRecord,
-  storeToLocalStorage
-} from "./utils/localStorageService";
-
-import "react-chat-elements/dist/main.css";
-import "./App.css";
-
-function App() {
-  const [hasName, setHasName] = useState(readRecord("username") !== null);
-  const [messages, setMessages] = useState([]);
-
-  const preparePastMessagesData = pastMessages => {
-    return pastMessages.map(msg => {
-      // Replace all properties 'type:image' with 'type: photo' as per react-chat-elements needs
-      if (msg.type && msg.type === "image") {
-        msg.type = "photo";
-      }
-
-      if (msg.data && msg.data.type && msg.data.type === "image") {
-        msg.data.type = "photo";
-      }
-
-      return msg;
-    });
-  };
-
-  const handleLogin = username => {
-    initChat()
-      .then(
-        loginChat(username)
-          .then(data => {
-            storeToLocalStorage("username", username);
-            setHasName(data.uid === username);
-
-            fetchChatGroupConversations().then(pastMessages => {
-              setMessages(preparePastMessagesData(pastMessages));
-            });
-            return true;
-          })
           .catch(err => {
             console.log("No connection to the Chat API", err);
             storeToLocalStorage("username", null);
